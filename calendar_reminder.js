6f2b8e6233f5853c620ede3216905737d2a5e1f4
Curt Toppel <curt@Curts-MacBook-Air.local>
2017-01-25 23:56:35 -0800

    complete

--- calendar_reminder.js
+++ calendar_reminder.js
@@ -1,9 +1,40 @@
-// var app = chrome.runtime.getBackgroundPage();
+let button = document.getElementById('button');
 
-function hello() {
-    chrome.tabs.executeScript({
-        file: 'alert.js'
-    }); 
+// button.addEventListener('click', setTimeout(() => {
+//   let w = 440;
+//   let h = 220;
+//   let left = (screen.width / 2) - (w / 2);
+//   let top = (screen.height / 2) - (h / 2);
+//
+//   chrome.windows.create({
+//     type: 'popup',
+//     focused: true,
+//     url: 'popup.html',
+//     width: w,
+//     height: h,
+//     left: left,
+//     top: top
+//   });
+//
+// }, 5000));
+
+function saveChanges() {
+  console.log('hi');
+  // Get a value saved in a form.
+  let sel = document.getElementById('calendar');
+  let theValue = sel.options[sel.selectedIndex].value;
+  console.log(theValue);
+  // Check that there's some code there.
+  if (!theValue) {
+    message('Error: No value specified');
+    return;
+  }
+  // Save it using the Chrome extension storage API.
+  chrome.storage.sync.set({
+    'value': theValue
+  }, function () {
+    // Notify that we saved.
+    message('Settings saved');
+  });
 }
-
-document.getElementById('clickme').addEventListener('click', hello);
+saveChanges();
